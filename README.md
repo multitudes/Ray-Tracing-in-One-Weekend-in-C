c0ef5a52d54a812eec2155a9631a31a87e48f524
lbrusa <lbrusa@student.42berlin.de>
2024-06-18 17:21:18 +0200

    FEAT adding calculation for view port

--- README.md
+++ README.md
@@ -39,6 +39,23 @@ when compiling I will need to use the $(INCLUDE) and $(LIBS) flags to include th
 
 
 ## Raytracing
+At its core, a ray tracer sends rays through pixels and computes the color seen in the direction of those rays. The involved steps are
+
+    Calculate the ray from the “eye” through the pixel,
+    Determine which objects the ray intersects, and
+    Compute a color for the closest intersection point.
+
+## aspect ratio
+A 16∶9 aspect ratio means that the ratio of image width to image height is 16∶9.  
+For a practical example, an image 800 pixels wide by 400 pixels high has a 2∶1 aspect ratio. 
+
+## The Viewport
+The viewport is a virtual rectangle in the 3D world that contains the grid of image pixel locations. If pixels are spaced the same distance horizontally as they are vertically, the viewport that bounds them will have the same aspect ratio as the rendered image. The distance between two adjacent pixels is called the pixel spacing, and square pixels is the standard. 
+We'll initially set the distance between the viewport and the camera center point to be one unit. This distance is often referred to as the focal length.  
+While our 3D space has the conventions above, this conflicts with our image coordinates, where we want to have the zeroth pixel in the top-left and work our way down to the last pixel at the bottom right. This means that our image coordinate Y-axis is inverted: Y increases going down the image. 
+ We'll also have the y-axis go up, the x-axis to the right, and the negative z-axis pointing in the viewing direction. (This is commonly referred to as right-handed coordinates.)
+
+
 
 ## links
 - [Raytracing in one weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)  
--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@
 #include <unistd.h>
 
 #include "utils.h"
-
+#include "vector.h"
+#include "color.h"
+#include "ray.h"
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -14,6 +16,7 @@ typedef struct s_params
 {
 	void *mlx;
 	mlx_image_t *img;
+	double aspect_ratio;
 } t_params;
 
 /*
@@ -25,6 +28,11 @@ void draw(void *param)
 	(void)params;
 }
 
+void ray_color(t_color *color, t_ray *r)
+{
+	(void)r;
+	init_vec3(color, 0, 0, 0);
+}
 
 
 
@@ -38,6 +46,42 @@ int main(int argc, char **argv)
 	
 	// for the book course we create a ppm image
 	create_ppm_image("test.ppm", WIDTH, HEIGHT);
+
+	params.aspect_ratio = 16 / 9;
+	
+	// viewport
+	int image_width = 400;
+	int image_height = (int)(image_width / params.aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
+	double viewport_height = 2.0;
+	double viewport_width = viewport_height * ((double)image_width/image_height);
+	double focal_length = 1.0;
+	t_vec3 camera_center;
+	init_vec3(&camera_center, 0, 0, 0);
+
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+	t_vec3 viewport_u;
+	init_vec3(&viewport_u, viewport_width, 0, 0);
+	t_vec3 viewport_v;
+	init_vec3(&viewport_v, 0, -viewport_height, 0); 
+
+	t_vec3 pixel_delta_u;
+	vec3multscalar(&pixel_delta_u, &viewport_u, 1.0 / (image_width));
+	t_vec3 pixel_delta_v;
+	vec3multscalar(&pixel_delta_v, &viewport_v, 1.0 / (image_height));
+
+	// location of the upper left corner of the viewport
+	t_vec3 viewport_upper_left;
+	t_vec3 viewport_half_up_left;
+	init_vec3(&viewport_half_up_left, -viewport_width / 2, -viewport_height / 2, -focal_length );
+	vec3substr(&viewport_upper_left, &camera_center , &viewport_half_up_left);
+	t_vec3 pixel00_loc;
+	t_vec3 pixel_delta;
+	vec3add(&pixel_delta, &pixel_delta_u, &pixel_delta_v);
+	vec3negate(&pixel_delta, &pixel_delta);
+	vec3divide(&pixel_delta, &pixel_delta, 2);
+	vec3add(&pixel00_loc, &viewport_upper_left, &pixel_delta);
 
 
 	params.mlx = mlx_init(WIDTH, HEIGHT, "in a weekend!", true);
